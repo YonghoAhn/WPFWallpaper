b38500a589648a9f42eb02895569c795d046c62b
YonghoAhn <nanhosoft@gmail.com>
2017-11-08 02:02:08 +0900

    .

--- WPFWallpaper/Models/YoutubeSearchModel.cs
+++ WPFWallpaper/Models/YoutubeSearchModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace WPFWallpaper.Models
     class YoutubeSearchModel
     {
         public string Name { get; set; }
-        public string Path { get; set; }
+        public Image Path { get; set; }
         public string ID   { get; set; }
         public string Title{ get; set; }
         public string Desc { get; set; }
--- WPFWallpaper/Pages/YoutubePage.xaml.cs
+++ WPFWallpaper/Pages/YoutubePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System.Collections.Generic;
+using System.Net;
 using System.Windows.Controls;
 using WPFWallpaper.Models;
 namespace WPFWallpaper.Pages
@@ -14,7 +17,34 @@ namespace WPFWallpaper.Pages
         {
             InitializeComponent();
             YoutubeSearchListbox.ItemsSource = YoutubeList;
-            YoutubeList.Add(new YoutubeSearchModel() { Name = "Test", Desc="Test", ID="Test", Path="Test", Title="Test" });
+            YoutubeList.Add(new YoutubeSearchModel() { Name = "Test", Desc="Test", ID="Test", Path=null, Title="Test" });
+        }
+
+        private async void YoutubeSearchButton_ClickAsync(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var YoutubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyBgwr13tNn2BL5TbKLvT6e_kCR8egeQ5og", // 키 지정
+                ApplicationName = "wallpaperengine"
+            });
+            var request = YoutubeService.Search.List("snippet");
+            request.Q = YoutubeSearchTextBox.Text;  //Search Keyword
+            request.MaxResults = 25;
+            var result = await request.ExecuteAsync();
+            foreach(var item in result.Items)
+            {
+                if(item.Id.Kind == "youtube#video")
+                {
+                    var req = WebRequest.Create(item.Snippet.Thumbnails.High.Url);
+                    using (var r = req.GetResponse())
+                    {
+                        using (var stream = r.GetResponseStream())
+                        {
+                            var img = System.Drawing.Image.FromStream(stream: stream);
+                            
+                        }
+                    }
+            }
         }
     }
 }
