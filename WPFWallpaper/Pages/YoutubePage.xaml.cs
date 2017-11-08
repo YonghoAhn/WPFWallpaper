c93979475a938a83c93fe75e1b6605a19899b1a8
YonghoAhn <hrh1110@naver.com>
2017-11-08 12:28:08 +0900

    Youtube Layout

--- WPFWallpaper/Pages/YoutubePage.xaml.cs
+++ WPFWallpaper/Pages/YoutubePage.xaml.cs
@@ -14,11 +14,29 @@ namespace WPFWallpaper.Pages
     /// </summary>
     public partial class YoutubePage : Page
     {
+        static ObservableCollection<YoutubeSearchModel> YoutubeCollection = new ObservableCollection<YoutubeSearchModel>();
+
         public YoutubePage()
         {
             InitializeComponent();
         }
 
-        private async void YoutubeSearchButton_ClickAsync(object sender, System.Windows.RoutedEventArgs e) => YoutubeSearchListbox.ItemsSource = await YoutubeManager.SearchYoutubeAsync(YoutubeSearchTextBox.Text);
+        private async void YoutubeSearchButton_ClickAsync(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+               YoutubeCollection = await YoutubeManager.SearchYoutubeAsync(YoutubeSearchTextBox.Text);
+            YoutubeSearchListbox.ItemsSource = YoutubeCollection;
+        }
+
+        private void YoutubeSearchListbox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (YoutubeSearchListbox.SelectedItems.Count > 0)
+            {
+                YoutubeSearchModel model = YoutubeCollection[YoutubeSearchListbox.SelectedIndex];
+                SelectedImage.Source = model.Path;
+                SelectedTitle.Text = model.Title;
+                SelectedDesc.Text = model.Desc;
+            }
+        }
     }
 }
